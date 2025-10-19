5be1e2f60f16fe4cc15fbf39741a316501dd5920
Doralitze <doralitze@chaotikum.org>
2025-10-19 19:08:41 +0200

    add: navigation to mkdocs output

--- build.bash
+++ build.bash
@@ -4,8 +4,13 @@ mkdir -p build/docs
 mkdir -p build/site
 rm -rf build/docs/*
 rm -rf build/site/*
+
 cp -r Fish_Doku build/docs/
 cp -r Editor build/docs/
+cp README.md build/docs/
+
 rename 's/Readme/index/' build/docs/**/*.md
 rename 's/README/index/' build/docs/**/*.md
+mv build/docs/README.md build/docs/index.md
+
 mkdocs build
